4129cc75f61c931d894658461824e568cf5d356f
kateashley <kateashley01@gmail.com>
2024-02-09 18:49:37 -0500

    Add files via upload

--- R/data.R
+++ R/data.R
@@ -30,3 +30,89 @@
 #'@examples
 #' data(employees)
 "employees"
+
+
+
+#' Lending data set
+#'
+#' A dataset containing simulated data for a fictional company that is studying the
+#' factors that are associated with employee attrition (choosing to leave the firm).
+#'
+#' @format A data frame with 10,000 rows and 27 variables; each row represents
+#' one accepted loan application from the Lending Club platform. The 'default' variable indicates
+#' whether the applicant defaulted on the loan (0 = did not default; 1 = defaulted).
+#' For definitions of the remaining predictor variables (listed below), please refer to the data dictionary
+#' provided in the Lending Club case.
+#' \describe{
+#'   \item{id}{A unique identifier for the dataset}
+#'   \item{default}{A binary variable indicating whether the applicant defaulted on the loan (1) or not (0)}
+#'   \item{home_ownership}{}
+#'   \item{annual_inc}{}
+#'   \item{mort_acc}{}
+#'   \item{purpose}{}
+#'   \item{application_type}{}
+#'   \item{loan_amnt}{}
+#'   \item{installment}{}
+#'   \item{int_rate}{}
+#'   \item{loan_term}{}
+#'   \item{verification_status}{}
+#'   \item{dti}{}
+#'   \item{fico_range_low}{}
+#'   \item{fico_range_high}{}
+#'   \item{total_acc}{}
+#'   \item{total_bal_ex_mort}{}
+#'   \item{avg_cur_bal}{}
+#'   \item{total_bc_limit}{}
+#'   \item{open_acc}{}
+#'   \item{inq_last_6mths}{}
+#'   \item{tot_coll_amt}{}
+#'   \item{delinq_amnt}{}
+#'   \item{age_earliest_cred_mo}{}
+#'   \item{total_rec_prncp}{}
+#'   \item{pymnt_plan}{}
+#'   \item{debt_settlement_flag}{}
+#'   }
+#'
+#'@examples
+#' data(lending)
+"lending"
+
+
+#' Purchases data set
+#'
+#' A dataset containing simulated customer data representing purchase status (1 = made a purchase, 0 = did not
+#' make a purchase) and selected customer demographics (age, gender, and annual income).
+#'
+#' @format A data frame with 500 rows and 4 variables; each row represents
+#' one customer (simulated, for a fictional customer) with their demographic information
+#' and an indicator for whether they have made a purchase from the company.
+#' \describe{
+#'   \item{age}{Customer age in years}
+#'   \item{gender}{Customer gender; M = man, W = woman, X = Non-binary or other not listed}
+#'   \item{income}{Customer annual income in USD}
+#'   \item{purchase}{Binary variable indicating whether the customer made a purchase; 1 = yes, 0 = no}
+#'   }
+#'
+#'@examples
+#' data(purchases)
+"purchases"
+
+
+#' Purchases50 data set
+#'
+#' A dataset containing simulated customer data representing purchase status (1 = made a purchase, 0 = did not
+#' make a purchase) and selected customer demographics (age and annual income).
+#' This dataset is a subset of 'purchases' containing just 50 observations and two demographic variables.
+#'
+#' @format A data frame with 50 rows and 3 variables; each row represents
+#' one customer (simulated, for a fictional customer) with their demographic information
+#' and an indicator for whether they have made a purchase from the company.
+#' \describe{
+#'   \item{age}{Customer age in years}
+#'   \item{income}{Customer annual income in USD}
+#'   \item{purchase}{Binary variable indicating whether the customer made a purchase; 1 = yes, 0 = no}
+#'   }
+#'
+#'@examples
+#' data(purchases50)
+"purchases50"
